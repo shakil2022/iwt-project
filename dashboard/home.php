ab5ff699f4b22f330b141f84f71d881b97158009
Md Shakil Ahmed <shakil.stu2018@juniv.edu>
2022-03-12 13:38:54 +0600

    Update home.php

--- dashboard/home.php
+++ dashboard/home.php
@@ -6,8 +6,8 @@
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Ju Medical Center</title>
-     <!-- <link rel="shortcut icon" href="https://juniv.edu/images/favicon.ico"> -->
+     <title>Hall Management System</title>
+     <link rel="shortcut icon" href="https://juniv.edu/images/favicon.ico">
      <!-- fonts -->
      <link rel="preconnect" href="https://fonts.googleapis.com">
      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
